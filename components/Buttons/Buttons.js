9671a7c16fa17dad094e5f1ffa58f8a059d3220c
Michael Trew <mtrew2015@gmail.com>
2018-09-08 02:37:48 -0400

    completed

--- components/Buttons/Buttons.js
+++ components/Buttons/Buttons.js
@@ -1,70 +1,84 @@
 class ButtonLink {
     constructor(element) {
-        this.element;
-
-        //Get the custom dataset attribute on the button link
-        this.data = ;
-            console.log(`this.data is ${this.data}`)
-            //expect 1, 2, 3, 4 
-
-        //Using the custom dataset attribute, access its associated
-        //button info element
-        this.buttonInfo = ;
-            console.log(`this.buttonInfo is ${this.buttonInfo}`)
-            //expect this.buttonInfo is [object HTMLDivElement]
-
-        //Using the buttonInfo element, create a new instance of the
-        //ButtonInfo class
-
-        this.buttonItem = ;
-            console.log(`this.buttonItem is ${this.buttonItem}`)
-            //expect this.buttonItem is [object Object]
-
-        //Add a click handler to the button reference and call
-        //the select method on click
-
+      this.element = element;
+  
+      //Get the custom dataset attribute on the button link
+      this.data = this.element.dataset.button;
+      console.log(`this.data is ${this.data}`);
+      //expect 1, 2, 3, 4
+  
+      //Using the custom dataset attribute, access its associated
+      //button info element
+      this.buttonInfo = document.querySelector(
+        `.button-info[data-button="${this.data}"]`
+      );
+      console.log(`this.buttonInfo is ${this.buttonInfo}`);
+      //expect this.buttonInfo is [object HTMLDivElement]
+  
+      //Using the buttonInfo element, create a new instance of the
+      //ButtonInfo class
+  
+      this.buttonItem = new ButtonInfo(this.buttonInfo);
+      console.log(`this.buttonItem is ${this.buttonItem}`);
+      //expect this.buttonItem is [object Object]
+  
+      //Add a click handler to the button reference and call
+      //the select method on click
+  
+      this.element.addEventListener("click", () => {
+        this.select();
+      });
     }
-
+  
     select() {
-        //Get all of the elements with the button class
-        const buttons = ;
-
-        //Create an array. Use forEach to remove the 'button-selected'
-        //class from each element
-
-
-        //Add a class named "button-selected" to this element
-
-        //Call the select method on the item associated with this link
-
+      //Get all of the elements with the button class
+      const buttons = document.querySelectorAll(".button");
+  
+      //Create an array. Use forEach to remove the 'button-selected'
+      //class from each element
+      Array.from(buttons).forEach(function(item) {
+        item.classList.remove("button-selected");
+      });
+      this.element.classList.add("button-selected");
+  
+      this.buttonItem.select();
+  
+      //Add a class named "button-selected" to this element
+  
+      //Call the select method on the item associated with this link
     }
-}
-
-class ButtonInfo {
+  }
+  
+  class ButtonInfo {
     constructor(element) {
-        this.element = ;
+      this.element = element;
     }
-
-    select () {
-        //Select all button-info elements from the DOM
-        const buttonInfo = ;
-
-        //Remove the class "button-info-selected" from each element
-
-
-        //Add a class named "button-info-selected" to this element
-
+  
+    select() {
+      //Select all button-info elements from the DOM
+      const buttonInfo = document.querySelectorAll(".button-info");
+  
+      //Remove the class "button-info-selected" from each element
+      buttonInfo.forEach(function(item) {
+        item.classList.remove("button-info-selected");
+      });
+      this.element.classList.add("button-info-selected");
+  
+      //Add a class named "button-info-selected" to this element
     }
-}
-
-//START HERE
-// Create a reference to all the ".button" class elements
-let links =  ;
-
-//Iterate through an array with a new instance of the ButtonLink
-//class for each item
-
-
-//LAST: Once you have created an array of Tablink instances, call
-//select() on the first item in the array
-
+  }
+  
+  //START HERE
+  // Create a reference to all the ".button" class elements
+  let links = document.querySelectorAll(".button");
+  
+  //Iterate through an array with a new instance of the ButtonLink
+  //class for each item
+  links = Array.from(links).map(item => {
+    return new ButtonLink(item);
+  });
+  
+  //LAST: Once you have created an array of Tablink instances, call
+  //select() on the first item in the array
+  links[0].select();
+  
--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -2,32 +2,34 @@ class Menu {
     constructor(element) {
 
         //Assign this.element to the menu element
-        this.element = ;
+        this.element = element;
 
         //Get the element with the '.menu-button' class 
-        this.button = ;
+        this.button = this.element.querySelector('.menu-button');
 
         //Assign the reference to the '.menu-content' class found
         //in the menu element
-        this.content = ;
+        this.content = this.element.querySelector('.menu-content');
 
         //Add a click handler to the button reference and call the 
         //toggleContent method
 
-
+        this.button.addEventListener('click', ()=>{
+            this.toggleContent();
+        })
     }
 
     toggleContent() {
 
         //Toggle the '.menu-hidden' class off and on
-        
+        this.content.classList.toggle('menu-hidden');
     }
 }
 
 //Get the '.menu' element
 
-let menu = ;
+let menu = document.querySelectorAll('.menu');
 
 //Map the element with a new instance of Menu 
 
-menu = Array.from().map();
+menu = Array.from(menu).map( item => new Menu(item));
